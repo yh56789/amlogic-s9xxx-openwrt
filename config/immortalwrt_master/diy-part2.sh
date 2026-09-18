e28b24ff4c90515240d6518069c82d3ca2db5914
yh56789 <yh567890@gmail.com>
2026-09-18 10:52:01 +0800

    Update diy-part2.sh

--- config/immortalwrt_master/diy-part2.sh
+++ config/immortalwrt_master/diy-part2.sh
@@ -47,10 +47,13 @@ fi
 #
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
+rm -rf package/feeds/luci/luci-app-dae
+rm -rf package/feeds/luci/luci-app-daed
+rm -rf package/feeds//packages/sing-box
 git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/kenzok8/openwrt-daede.git package/luci-app-daede
 git clone https://github.com/easytier/luci-app-easytier.git package/luci-app-easytier
-#
+git clone https://github.com/SagerNet/sing-box.git package/feeds//packages/sing-box
 # Apply patches
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
